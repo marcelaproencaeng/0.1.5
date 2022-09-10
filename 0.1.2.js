dbd827da6a6068f213824cea578f70a008c7072f
marcelaproencaeng <104868735+marcelaproencaeng@users.noreply.github.com>
2022-09-09 21:16:38 -0300

    1 file modified

--- 0.1.2.js
+++ 0.1.2.js
@@ -1,29 +1,26 @@
-//Adapte o Exercício 0.1.2 para que o desconto seja aplicado para um valor de no máximo 100 reais independente do tipo de desconto. Além disso, adicione um quarto parâmetro que irá representar se essa é a primeira compra desse usuário no estabelecimento. Dê um desconto de 5% se for a primeira compra. 
+//Adapte o Exercício 0.1.2 para que o desconto seja aplicado para um valor de no máximo 100 reais independente do tipo de desconto. Além disso, adicione um quarto parâmetro que irá representar se essa é a primeira compra desse usuário no estabelecimento. Dê um desconto de 5% se for a primeira compra.
 
-function compra(valorCompra, temConvenio, cartaoLoja,primeiraCompra) {
+function compra(valorCompra, temConvenio, cartaoLoja) {
   let valorFinal = 0;
-  const descontoMax=100;
+  const descontoMax = 100;
 
   if (temConvenio && cartaoLoja) {
-    if((valorCompra*0.15>100)){
-      return valorCompra-100;
-    }else if(valorCompra*0.15<100){
-      return valorCompra -100;
+    if (valorCompra * 0.15 > 100) {
+      return valorCompra - 100;
+    } else if (valorCompra * 0.15 < 100) {
+      return valorCompra;
     }
-      
+  }
   if (temConvenio || cartaoLoja) {
-    if((valorCompra* 0.1>100)){
-      return valorCompra-100;
-    }else if(valorCompra*0.10<100){
-      return valorCompra-100;
+    if (valorCompra * 0.1 > 100) {
+      return valorCompra - 100;
     }
-    if()
-    
+    return valorCompra - valorCompra * 0.1;
   } else {
     valorFinal = valorCompra;
     return valorCompra;
   }
 }
-console.log(compra(100, true, true));
-console.log(compra(100, false, true));
+console.log(compra(1100, true, true));
+console.log(compra(1000, false, true));
 console.log(compra(100, false, false));
